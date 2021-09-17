2213b41247ed9e890dcc317be574f7fc75f05f18
Morne Louw <morne.louw@saongroup.com>
2021-09-17 06:28:53 +0200

    Updated the placeholder to have a defaultProp value. Also set the onChange prop as requuired

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -12,14 +12,14 @@ import { Container } from 'react-bootstrap';
 // CSS
 import "./Editor.css";
 
-const Editor = ({ document, onChange }) => {
+const Editor = ({ document, onChange, placeholder }) => {
     const editor = useMemo(() => withReact(createEditor()), []);
 
     return (
         <Slate editor={editor} value={document} onChange={onChange}>
             <Container className={"editor-container"}>
                 <div className="editor">
-                    <Editable placeholder="Enter some rich text…" />
+                    <Editable placeholder={placeholder} />
                 </div>
             </Container>
         </Slate>
@@ -31,7 +31,12 @@ Editor.propTypes = {
     /** The  */
     document: PropTypes.array,
     /** Event to change the task to the Archived state */
-    onChange: PropTypes.func,
+    onChange: PropTypes.func.isRequired,
+}
+
+// MRL: Describe default values for any props. Data type for these will be inferred
+Editor.defaultProps = {
+    placeholder: 'Enter some text...'
 }
 
 export default Editor;
